b62c060616eaea9e56b44b4c23fb2cc15e059414
yahavb <ybiran@colostate.edu>
2016-08-02 15:24:31 -0700

    add support for weibull-based timer

--- loader/bin/simu-analysis.sh
+++ loader/bin/simu-analysis.sh
@@ -1,4 +1,4 @@
 curl http://104.154.70.81/map.php?cmd=cnt_by_keys\&key=TS*TYPE*> ~/Downloads/1.csv
-cat 1.csv | egrep '^TS*'| sed 's/TS//g' | sed 's/TYPE//g'| sed 's/-_/,/g' | sort > ~/Downloads/2.csv
-
+echo Time,Type,Count>~/Downloads/2.csv
+cat ~/Downloads/1.csv | egrep '^TS*'| sed 's/TS//g' | sed 's/TYPE//g'| sed 's/-_/,/g' | sort >> ~/Downloads/2.csv
 echo "File ~/Downloads/2.csv is ready for analysis"
